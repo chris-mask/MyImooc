190307e313000c53b01314001f9cc28afbae6065
chris-mask <635742310@qq.com>
2019-10-30 22:24:40 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,29 +1,103 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+// import VueLazyLoad from 'vue-lazyload'
+// // import {sum,minus} from "./util";
+// // console.log(`sum:${sum(1,6)}`);
+
+// import infiniteScroll from 'vue-infinite-scroll'
+// import {currency} from './util/currency'
+
+// Vue.use(infiniteScroll);
+// Vue.config.productionTip = false;
+
+// Vue.filter("currency",currency);
+
+
+// Vue.use(VueLazyLoad,{
+//   loading:"/static/loading-svg/loading-balls.svg"
+// });
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VueLazyLoad from 'vue-lazyload'
-// import {sum,minus} from "./util";
-// console.log(`sum:${sum(1,6)}`);
-
-import infiniteScroll from 'vue-infinite-scroll'
+import axios from 'axios'
+import Vuex from 'vuex'
+import VueLazyload from 'vue-lazyload'
+import infiniteScroll from  'vue-infinite-scroll'
 import {currency} from './util/currency'
 
+import './assets/css/base.css'
+import './assets/css/checkout.css'
+import './assets/css/product.css'
+
 Vue.use(infiniteScroll);
-Vue.config.productionTip = false;
+Vue.use(Vuex);
+Vue.use(VueLazyload, {
+  loading: 'static/loading-svg/loading-bars.svg',
+  try: 3 // default 1
+})
 
 Vue.filter("currency",currency);
+Vue.config.productionTip = false;
 
-
-Vue.use(VueLazyLoad,{
-  loading:"/static/loading-svg/loading-balls.svg"
+const store = new Vuex.Store({
+  state: {
+    nickName:'',
+    cartCount:0
+  },
+  mutations: {
+    //更新用户信息
+    updateUserInfo(state, nickName) {
+      state.nickName = nickName;
+    },
+    updateCartCount(state,cartCount){
+      state.cartCount += cartCount;
+    }
+  }
 });
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
-  components: { App },
-  template: '<App/>'
-})
+  mounted(){
+    this.checkLogin();
+    this.getCartCount();
+  },
+  methods:{
+    checkLogin(){
+      axios.get("users/checkLogin").then(res=> {
+        var res = res.data;
+        if (res.status == "0") {
+          this.$store.commit("updateUserInfo", res.result);
+        }else{
+          if(this.$route.path!="/goods"){
+            this.$router.push("/goods");
+          }
+        }
+      });
+    },
+    getCartCount(){
+      axios.get("users/getCartCount").then(res=>{
+        var res = res.data;
+        if(res.status=="0"){
+          this.$store.commit("updateCartCount",res.result);
+        }
+      });
+    }
+  },
+  template: '<App/>',
+  //render: h => h(App),
+  components: { App }
+});//.$mount('#app')
